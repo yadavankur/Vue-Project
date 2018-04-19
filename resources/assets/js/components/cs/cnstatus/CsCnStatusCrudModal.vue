aaf76749e1ccbb090b3356fd48e7692de4a650c5
manojkmishra <manojkm1980@gmail.com>
2018-04-19 17:51:40 +1000

    credit node vue files

---
+++ resources/assets/js/components/cs/cnstatus/CsCnStatusCrudModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <custom-modal :value="showPopup" @cancel="onClose" effect="fade">
+            <div slot="modal-header" class="modal-header">
+                <h4 class="modal-title"> {{ title }} </h4>
+            </div>
+            <div slot="modal-body" class="modal-body">
+                <div class="form-group">
+                    <bs-input label="STATUS" type="text" required  :maxlength="255" :icon="true" v-model="formData.STATUS"></bs-input>
+                    <bs-input label="Comment" type="textarea" :maxlength="255" :icon="true" v-model="formData.comment"></bs-input>
+                </div>
+            </div>
+            <div slot="modal-footer" class="modal-footer">
+                <button type="button" class="btn btn-success" @click="onClose">Cancel</button>
+                <button type="button" class="btn btn-primary" @click="OnSave">Save</button>
+            </div>
+        </custom-modal>
+    </div>
+</template>
+<script>
+    import Vue from 'vue'
+    import { mapState } from 'vuex'
+    import modal from 'vue-strap/src/Modal'
+    import input from 'vue-strap/src/Input'
+    export default 
+    {  computed: 
+           { ...mapState({ showPopup: state => state.csticketcnstatus.showPopup,//---------add---6
+                          statusData: state=> state.csticketcnstatus.cnstatusData,  
+                        }),   
+            },
+       data ()  {  return {  title: '',  formData: {     id: '',    STATUS: '',   comment: '', }    }   },
+       created() {  console.log('/cs/csstatus/CsStatusCrudModal.vue---- created.')  },
+       components: {  'custom-modal': modal, 'bs-input': input,  },
+       mounted() { console.log('/cs/csstatus/CsStatusCrudModal.vue--- mounted. statusData=', this.statusData) },
+       methods: 
+           {
+               OnSave() //---------------on save while adding and edit----coming from action=Add in  onClickNew() in statelistview
+                { console.log('/cs/csstatus/CsStatusCrudModal.vue----OnSave_click'); //-------add----9
+                  let payload = {  isShow: false,  data: this.formData, }; //isshow false after save pressed
+                  if (this.statusData.action === 'Add')// add new state
+                     { this.$store.dispatch('cscnstatusshowpopup', payload); //this is to disable popup after save pressed ie  isshow- false
+                       this.$store.dispatch('cscnstatusadd', this.formData) //--add-10--send the status form fields to store file
+                        .then((response) => {})     .catch((error) => {});
+                     } 
+                  else if (this.statusData.action === 'Edit')// edit---8
+                   { 
+                       this.$store.dispatch('cscnstatusshowpopup', payload);  
+                       this.$store.dispatch('updatecnstatus', this.formData)
+                        .then((response) => {})     .catch((error) => {});
+                   }
+                  else  {     }// error
+                },
+               onClose() {  console.log('/cs/csstatus/CsStatusCrudModal.vue-----onClose'); ////---------add---8
+                           let payload = {   isShow: false,   data: this.formData,   };
+                           this.$store.dispatch('cscnstatusshowpopup', payload);
+                           this.resetFormData();
+                        },
+               resetFormData() {  this.formData = { id: '',  STATUS: '',  comment: ''  }; }
+
+           },
+           watch: {  statusData() 
+                      {  console.log('/cs/csstatus/CsStatusCrudModal.vue-+++++++ statusData changed =', this.statusData);
+                         if (this.statusData && this.statusData.action === 'Add')  ////---------add---7
+                            {   this.resetFormData();   this.title = 'Adding New Status';  
+                                console.log('/cs/csstatus/CsStatusCrudModal.vue-+++add form is open now_just before save is pressed');
+                            }
+                          else if (this.statusData && this.statusData.action === 'Edit')//-----edit 7
+                           {   this.resetFormData();  
+                               this.title = 'Editing the STATUS';
+                               console.log('/cs/csstatus/CsStatusCrudModal.vue-+++edit form is open now_just before save is pressed');
+                               this.formData.id = this.statusData.data.id;
+                               this.formData.STATUS = this.statusData.data.STATUS;
+                               this.formData.comment = this.statusData.data.comment;
+                           }
+                       }
+               }
+    }
+
+    </script>
---
+++ resources/assets/js/components/cs/cnstatus/CsCnTicketStatus.vue
@@ -0,0 +1,30 @@
+<template> <div id="test-list"> <div class="main"><div class="container">
+                <test-crud-modal></test-crud-modal>
+                <div class="row"> <div class="col-md-12"> <div class="panel panel-primary">
+                            <div class="panel-heading"><div class="test-title"> Test list </div> </div>
+                            <div id="testlist" class="panel-collapse collapse in table-responsive">
+                                <ul class="test-group">  <li id="tests-list" class="list-group-item">
+                                        <test-list-view :ticketstatus="ticketstatus"></test-list-view>
+                       </li> </ul> </div></div></div> </div> </div>  </div></div>
+</template>
+<script>
+    import Vue from 'vue'
+    import { mapGetters, mapState, mapActions} from 'vuex'
+    import TestListView from './CsCnStatusListView.vue'
+    import TestCrudModal from './CsCnStatusCrudModal.vue'
+export default 
+    {   data () { return {   }  },
+        computed:  { ...mapGetters({ ticketstatus: 'ticketcnstatustablegetter',  }), ...mapState({    }),  },
+        created() 
+            {//console.log('components/New1/NewView1.vue-hehe=', stateNodes1);
+              console.log('cs/csstatus/csticketstatus.vue- Component created.');
+              this.$store.dispatch('getticketcnstatustable')
+                .then((response) => { console.log('cs/csstatus/csticketstatus.vue-- created response=', response);  })
+                .catch((error) => { console.error('cs/csstatus/csticketstatus.vue-- created error=', error);   });
+            },
+        components: { 'test-crud-modal': TestCrudModal, 'test-list-view': TestListView,  },
+        updated() { console.log('cs/csstatus/csticketstatus.vue--- Component updated.')   },
+        mounted() { console.log('cs/csstatus/csticketstatus.vue--- Component mounted.')  },
+    }
+
+    </script>
